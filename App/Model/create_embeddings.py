26b3861e812fc638dc06cfaa8de7949c4d520f77
Jacob <jakubwiesniak102@gmail.com>
2024-06-09 21:40:40 +0200

    First Prototype

---
+++ App/Model/create_embeddings.py
@@ -0,0 +1,51 @@
+import pandas
+import translators as ts
+from transformers import BertTokenizer, BertModel
+import torch
+import chromadb
+from chromadb import Documents, EmbeddingFunction, Embeddings
+
+tokenizer = BertTokenizer.from_pretrained('sentence-transformers/paraphrase-TinyBERT-L6-v2')
+model = BertModel.from_pretrained('sentence-transformers/paraphrase-TinyBERT-L6-v2')
+
+# Load the model weights from the file via loading the state_dict
+model.load_state_dict(torch.load('Data/model_weights.pth'))
+
+
+# Script that translates the text from polish to english and then tokenizes and embeds it
+def translate_token_embed(text):
+    translated_text = ts.translate_text(query_text=text, translator='google', from_language='pl', to_language='en')
+    tokenized_text = tokenizer.encode_plus(translated_text, add_special_tokens=True, return_tensors='pt')
+    embedded_text = model(**tokenized_text)
+    return embedded_text.last_hidden_state.mean(dim=1).flatten().tolist()
+
+
+# Custom embedding function
+class MyEmbeddingFunction(EmbeddingFunction):
+    def __call__(self, input: Documents) -> Embeddings:
+        embeddings = [translate_token_embed(doc) for doc in input]
+        return embeddings
+
+
+def get_embedding_function():
+    return MyEmbeddingFunction()
+
+
+
+if __name__ == '__main__':
+    # create instance of chroma
+    client = chromadb.PersistentClient(path="Data/Chroma")
+
+    # Deleting collection
+    client.delete_collection(name="embeddings")
+
+    # Creating collection
+    collection = client.create_collection(name="embeddings", embedding_function=get_embedding_function())
+
+    # Load data
+    df = pandas.read_csv('Data/unaugmented_data.csv')
+
+    # Add document
+    collection.add(ids=df['category'].tolist(), documents=df['text'].tolist())
+
+    print(collection)
--- App/app.py
+++ App/app.py
@@ -1,10 +1,71 @@
-from flask import Flask, render_template
+import pandas as pd
+from flask import Flask, render_template, request
+import chromadb
+from chromadb import Documents, EmbeddingFunction, Embeddings
+import pandas
+import create_embeddings as ce
 
 app = Flask(__name__)
+client = chromadb.PersistentClient(path="Model/Data/Chroma")
 
+# Chroma instance initialization
+
+
+# Endpoints initialization
 @app.route('/')
 def home():
     return render_template('index.html')
 
+
+@app.route('/tutorial')
+def tutoria():
+    return render_template('tutorial.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route('/submit', methods=['POST'])
+def submit():
+
+    prompt = request.form['prompt']
+
+    collection = client.get_collection(name="embeddings", embedding_function=ce.get_embedding_function())
+
+    print(collection)
+    results = collection.query(
+        query_texts=[prompt],  # Chroma will embed this for you
+        n_results=1  # how many results to return
+    )
+
+    print(results)
+
+    outcome = pd.read_csv('Model/Data/outcome.csv', sep=';')
+
+    ids = results['ids']
+    documents = results['documents']
+    # for "unpacking" list
+    ids = ''.join([str(item) for sublist in ids for item in sublist])
+    documents = ''.join([str(item) for sublist in documents for item in sublist])
+    ids = ids[:-2]
+    print(f"ids {ids}")
+    outcome_result = outcome.loc[outcome['game'] == ids]
+
+    result  = []
+    result.append(outcome[outcome['game'] == ids]['style'].values)
+    result.append(outcome[outcome['game'] == ids]['style_description'].values)
+    result.append(ids)
+    result.append(documents)
+
+    print(f"outcome{outcome_result}")
+
+    return render_template('index.html', result=result)
+
+
+
 if __name__ == '__main__':
+    # build database
+    ce.build_database()
     app.run(debug=True)
